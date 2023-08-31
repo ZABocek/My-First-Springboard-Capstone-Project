152efe9a804b9c1637fb2fc2864f95fbf39bcea0
ZABocek <zabocek@gmail.com>
2023-08-31 19:13:45 +0200

    modified forms.py

--- forms.py
+++ forms.py
@@ -1,15 +1,14 @@
 """Forms for cocktail app."""
 
-import email_validator
 from wtforms import SelectField
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SelectField, TextAreaField, BooleanField, PasswordField
+from wtforms import validators, StringField, EmailField, IntegerField, SelectField, TextAreaField, BooleanField, PasswordField
 from wtforms.validators import InputRequired, Length, NumberRange, URL, Optional
 
 class RegisterForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired()])
     password = PasswordField("Password", validators=[InputRequired(), Length(min=8)])
-    email = StringField('E-mail', validators=[InputRequired(), email_validator()])
+    email = EmailField('E-mail', validators=[InputRequired()])
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired()])
