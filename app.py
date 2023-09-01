ac8309e399a9e62341f12fb4eb321c937edf47cf
ZABocek <zabocek@gmail.com>
2023-09-01 13:27:56 +0200

    modified app.py

--- app.py
+++ app.py
@@ -31,6 +31,12 @@ connect_db(app)
 @app.route("/")
 def root():
     """Homepage: redirect to /cocktails."""
+    print("****************session************")
+    print(session["username"])
+    print(session["user_id"])
+
+    print("****************session************")
+
 
     return render_template("index.html")
 
