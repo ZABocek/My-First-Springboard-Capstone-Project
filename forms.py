b8886ce1a4c0dcda11637c1d877ffe2a8c9d90f4
ZABocek <zabocek@gmail.com>
2023-09-07 11:12:00 +0200

    added id variable to RegisterForm in forms.py

--- forms.py
+++ forms.py
@@ -2,13 +2,14 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, IntegerField, BooleanField, PasswordField, TextAreaField
-from wtforms.validators import DataRequired, Email, InputRequired, Length, Optional
+from wtforms.validators import DataRequired, Email, InputRequired, Length, EqualTo, Optional
 
 class RegisterForm(FlaskForm):
     """Form for registering a user."""
-
+    id = IntegerField("User ID")
     username = StringField("Username", validators=[InputRequired()])
-    password = PasswordField("Password", validators=[InputRequired(), Length(min=8)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=8), EqualTo('confirm', message='Passwords must match') ])
+    confirm  = PasswordField('Repeat Password')
     email = EmailField("email", validators=[DataRequired(), Email()])
 
 
