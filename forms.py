64ac24a86b928337799d12adc4a423f4a64c8b0d
Zdenek Andrew Bocek <zabocek@gmail.com>
2023-09-15 17:16:35 +0300

    fixed

--- forms.py
+++ forms.py
@@ -1,44 +1,4 @@
-<<<<<<< HEAD
-"""Forms for cocktail app."""
 
-from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, IntegerField, BooleanField, PasswordField, TextAreaField
-from wtforms.validators import DataRequired, Email, InputRequired, Length, EqualTo, Optional
-
-class RegisterForm(FlaskForm):
-    """Form for registering a user."""
-    email = EmailField("email", validators=[DataRequired(), Email()])
-    username = StringField("Username", validators=[InputRequired()])
-    password = PasswordField("Password", validators=[InputRequired(), Length(min=8), EqualTo('confirm', message='Passwords must match') ])
-    confirm  = PasswordField('Repeat Password')
-   
-
-class LoginForm(FlaskForm):
-    """Form for registering a user."""
-
-    username = StringField("Username", validators=[InputRequired()])
-    password = PasswordField("Password", validators=[InputRequired()])
-
-class IngredientForm(FlaskForm):
-    id = IntegerField("id", validators=[Optional()])
-    name = StringField("name", validators=[InputRequired()])
-    is_alcohol = BooleanField("Is it alcohol?", validators=[Optional()])
-
-class CocktailForm(FlaskForm):
-    id = IntegerField("Cocktail ID")
-    ingredient = TextAreaField("Ingredient Name", validators=[Optional()])
-    cocktailname = StringField("Cocktail Name", validators=[InputRequired()])
-    instructions = TextAreaField("Preparation Instructions", validators=[Optional()])
-
-class NewIngredientForCocktailForm(FlaskForm):
-    """Form for adding an ingredient to a cocktail."""
-
-class SearchIngredientsForm(FlaskForm):
-    """Form for searching music"""
-    name = StringField("Search for song or word on a song", validators=[InputRequired()] )
-
-class DeleteForm(FlaskForm):
-=======
 """Forms for cocktail app."""
 
 from flask_wtf import FlaskForm
@@ -76,5 +36,4 @@ class SearchIngredientsForm(FlaskForm):
     name = StringField("Search for song or word on a song", validators=[InputRequired()] )
 
 class DeleteForm(FlaskForm):
->>>>>>> f5a1523976fe65d7f4a8d08ee3f457bac830193e
     """Delete form -- this form is intentionally blank."""
