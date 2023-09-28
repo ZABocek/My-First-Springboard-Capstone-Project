7d84177a3cf8d4e59ef05c48ddd8c53886e3bd73
Zdenek Andrew Bocek <zabocek@gmail.com>
2023-09-28 10:06:09 +0300

    modified cocktaildb_api.py and it still doesn't work

--- cocktaildb_api.py
+++ cocktaildb_api.py
@@ -1,11 +1,29 @@
+# cocktaildb_api.py
 import requests
-from api_keys import KEY  # Import the API key from secrets.py
 
-BASE_URL = "https://www.thecocktaildb.com/api/json/v1"
+BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1"
 
-def get_cocktail_by_name(cocktail_name):
-    url = f"{BASE_URL}/{KEY}/search.php?s={cocktail_name}"  # Include the API key in the URL
+def search_ingredient(ingredient_name):
+    url = f"{BASE_URL}/search.php"
+    response = requests.get(url, params={"i": ingredient_name})
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+def list_ingredients():
+    url = f"{BASE_URL}/list.php?i=list"
     response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+def lookup_cocktail(cocktail_id):
+    url = f"{BASE_URL}/lookup.php"
+    response = requests.get(url, params={"i": cocktail_id})
     
     if response.status_code == 200:
         return response.json()
